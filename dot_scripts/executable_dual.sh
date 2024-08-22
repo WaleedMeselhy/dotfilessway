0882025f5cf19c2475b485996461de718f55275f
Waleed Meselhy <waleed_meselhy@eng.asu.edu.eg>
2024-08-22 09:42:15 +0300

    update configs

--- dot_scripts/executable_dual.sh
+++ dot_scripts/executable_dual.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-wlr-randr --output DP-4 --mode 1920x1080 --pos 4640,0 --transform 270 --scale 1.0  \
-    --output DP-3 --mode 3840x2160 --pos 1440,0 --transform normal --scale 1.2 \
-    --output DP-2 --mode 2560x1440  --pos 0,0 --transform 90 --scale 1.0 \
+wlr-randr --output DP-4 --mode 3840x2160 --pos 2923,0 --transform normal --scale 1.2  \
+    --output DP-2 --mode 2560x1440  --pos 1723,0 --transform 90 --scale 1.2 \
     --output eDP-1 --off
--- executable_dot_aliases.sh
+++ executable_dot_aliases.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 alias fixZshHistory="cat -n ~/.zhistory | sort -t ';' -uk2 | sort -nk1 | cut -f2- > ~/.zhistory"
